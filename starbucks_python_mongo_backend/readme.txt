461660add2281a54815c7d19ecfb757dde0013bb
amrutadh <amrutadh@gmail.com>
2017-04-14 23:51:55 -0700

    added elasticbeanstalk steps

---
+++ starbucks_python_mongo_backend/readme.txt
@@ -0,0 +1,17 @@
+
+create requirements.txt having needed python packages
+
+Elastic beanstalk user for python application
+
+AKIAIAFVYDDXOU5DGV2Q : Access key 
+secret Access key : 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
+
+using aws eb cli:
+
+eb init : to create application - set basic info
+
+eb create : to create environment to deploy
+
+eb deploy :  to deploythe app on server
+
+eb open : open the url on browser
---
+++ starbucks_python_mongo_backend/starbucksAPIService.py
@@ -0,0 +1,51 @@
+from enum import Enum
+import uuid
+import datamanager
+
+class OrderStatus(Enum):
+    PLACED = 1
+    PAID = 2
+    PREPARING = 3
+    SERVED = 4
+    COLLECTED = 5
+
+
+class StarbucksAPIService():
+
+    def postOrder(self,Order):
+        # POST THE ORDER IN THE DB
+        print("post order")
+        order_id = uuid.uuid1()
+        x = str(order_id)
+        datamanager.collection.insert_one({"order": Order})
+        return Order
+
+    def getOrder(self, id):
+        # POST THE ORDER IN THE DB
+        print("get order")
+        data = datamanager.collection.find_one({"order.id": id}, {"_id": 0})
+        return data
+
+    def putOrder(self, Order,id):
+        # POST THE ORDER IN THE DB
+        print("put order")
+        datamanager.collection.update({'id': id}, {"$set": Order}, upsert=False)  # updating the order
+        return {'status':'updated'}
+
+    def payOrder(self, id):
+        # POST THE ORDER IN THE DB
+        print("paid order")
+        datamanager.collection.update({'order.id': id}, {"$set": {'order.status': "Paid"}})
+        return {'status':'paid'}
+
+    def getOrders(self):
+        print("inside getOrders")
+        cursor = datamanager.collection.find()
+        for document in cursor:
+            print(document)
+        return {'status': 'listoders'}
+
+    def cancel_Order(order_id):
+        print("Cancelling order with id", order_id)
+        datamanager.collection.remove({"id": order_id})
+        return "Order Cancelled"
